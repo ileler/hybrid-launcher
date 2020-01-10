ab8e104388707ed4387986e2a451f1b20a84f6bd
ileler <ileler@qq.com>
2020-01-10 10:18:26 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "net"
+    "time"
+    "runtime"
+    "os/user"
+    "os/exec"
+    "net/http"
+    "io/ioutil"
+    "path/filepath"
+)
+
+func main() {
+    myself, error := user.Current()
+    if error != nil {
+        panic(error)
+    }
+    homedir := myself.HomeDir + "/.mst/"
+    pid := homedir + ".pid"
+    var addr string
+
+    if _, err := os.Stat(pid); err == nil {
+        data, err := ioutil.ReadFile(pid)
+        if err != nil {
+            panic(err)
+        }
+        addr = string(data)
+        req, err := http.NewRequest("HEAD", addr, nil)
+        if err != nil {
+            panic(err)
+        }
+        client := &http.Client{ Timeout: time.Second * 1 }
+        resp, err := client.Do(req)
+        if err == nil && resp.StatusCode == 200 {
+            go open(addr)
+            time.Sleep(time.Second * 1)
+            os.Exit(0)
+        }
+        os.Remove(pid)
+    }
+
+
+    http.HandleFunc("/api", func (w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "application/json")
+        fmt.Fprintf(w, "{\"hello\": \"world\"}")
+    })
+
+    fs := http.FileServer(http.Dir("static/"))
+    http.Handle("/", http.StripPrefix("/", fs))
+
+    listener, err := net.Listen("tcp", ":0")
+    if err != nil {
+        panic(err)
+    }
+
+    //fmt.Println("Using port:", listener.Addr().(*net.TCPAddr).Port)
+
+    addr = fmt.Sprintf("%s%d", "http://localhost:", listener.Addr().(*net.TCPAddr).Port)
+
+    if err := os.MkdirAll(filepath.Dir(pid), 0775); err != nil {
+        panic(err)
+    }
+    file, err := os.Create(pid)
+    defer file.Close()
+    file.WriteString(addr)
+
+    go open(addr)
+    panic(http.Serve(listener, nil))
+}
+
+// open opens the specified URL in the default browser of the user.
+func open(url string) error {
+    var cmd string
+    var args []string
+
+    switch runtime.GOOS {
+    case "windows":
+        cmd = "cmd"
+        args = []string{"/c", "start"}
+    case "darwin":
+        cmd = "open"
+    default: // "linux", "freebsd", "openbsd", "netbsd"
+        cmd = "xdg-open"
+    }
+    args = append(args, url)
+    return exec.Command(cmd, args...).Start()
+}
